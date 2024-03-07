8774cd0b193e9a8d3da450d68d353d193ef5f73e
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-07 21:47:00 +0530

    added features

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -3,25 +3,45 @@
 import Marquee from "./Marquee";
 
 const data = [
-	"RESPONSIVE DESIGN",
-	"USER-FRIENDLY INTERFACE",
-	"SCALABILITY",
-	"PERFORMANCE",
-	"CROSS-BROWSER COMBATIBILITY",
-	"SEARCH ENGINE OPTIMIZTION(SEO)",
+  "RESPONSIVE DESIGN",
+  "USER-FRIENDLY INTERFACE",
+  "SCALABILITY",
+  "PERFORMANCE",
+  "CROSS-BROWSER COMBATIBILITY",
+  "SEARCH ENGINE OPTIMIZTION(SEO)",
 ];
 export const Features = () => {
-	return (
-		<div className="h-screen relative w-full  text-white flex justify-center items-center">
-			<div className="absolute top-0 ">
-				<Marquee
-					word={`${data[0]}#${data[1]}#`}
-					size="100px"
-					initialDirection={1}
-					scroll={true}
-					speed={0.09}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="py-40 relative w-full  text-white flex justify-center  overflow-hidden items-center">
+      <div className=" w-screen top-0 flex flex-col gap-2 ">
+        <div className="w-fit">
+          <Marquee
+            word={`${data[0]}#${data[1]}#`}
+            size="100px"
+            initialDirection={1}
+            scroll={true}
+            speed={0.09}
+          />
+        </div>
+        <div className="w-fit">
+          <Marquee
+            word={`${data[2]}#${data[3]}#${data[2]}#${data[3]}#`}
+            size="100px"
+            initialDirection={1}
+            scroll={true}
+            speed={0.1}
+          />
+        </div>
+        <div className="w-fit">
+          <Marquee
+            word={`${data[4]}#${data[5]}#`}
+            size="100px"
+            initialDirection={1}
+            scroll={true}
+            speed={0.12}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
