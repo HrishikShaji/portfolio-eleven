dfcd65a56c321d6c8e554d85d2f036a67c130797
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-08 18:11:28 +0530

    yValuesbugs

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,69 +1,88 @@
+"use client";
+
 import Image from "next/image";
 import { data } from "../lib/data";
-import { useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useEffect, useRef } from "react";
 import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
 export const Projects = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const projectRefs = useRef<(HTMLDivElement | null)[]>([]);
+	const containerRef = useRef<HTMLDivElement>(null);
+	const projectRefs = useRef<(HTMLDivElement | null)[]>([]);
+	const titleRef = useRef<HTMLHeadingElement>(null);
 
-  const yValues = [
-    -100, -50, -30, -300, -220, -120, -280, -160, -240, -10, -110, -240,
-  ];
+	const yValues = [
+		-250, -500, -350, -500, -700, -620, -280, -160, -240, -400, -300, -240,
+	];
 
-  useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: containerRef.current,
-          start: "top bottom",
-          end: "bottom top",
-          scrub: true,
-        },
-      });
+	useEffect(() => {
+		let ctx = gsap.context(() => {
+			const tl = gsap.timeline({
+				scrollTrigger: {
+					trigger: containerRef.current,
+					start: "top bottom",
+					end: "bottom top",
+					markers: true,
+					scrub: 1,
+				},
+			});
+			tl.to(projectRefs.current, { y: (i) => yValues[i] }, 0);
+			const height = containerRef.current?.getBoundingClientRect().height;
+			if (height) {
+				ScrollTrigger.create({
+					trigger: titleRef.current,
+					pin: titleRef.current,
+					start: "top 20%",
+					end: `+=${height}`,
+					scrub: true,
+				});
+			}
+		});
 
-      tl.to(projectRefs.current, { y: (i) => yValues[i] }, 0);
-    });
+		return () => ctx.revert();
+	}, []);
 
-    return () => ctx.revert();
-  }, []);
+	const coordinates = [
+		{ top: "500px", left: "0px" },
+		{ top: "500px", left: "500px" },
+		{ top: "500px", left: "1000px" },
+		{ top: "1000px", left: "0px" },
+		{ top: "1000px", left: "500px" },
+		{ top: "1000px", left: "1000px" },
+		{ top: "1500px", left: "0px" },
+		{ top: "1500px", left: "500px" },
+		{ top: "1500px", left: "1000px" },
+		{ top: "2000px", left: "0px" },
+		{ top: "2000px", left: "500px" },
+		{ top: "2000px", left: "1000px" },
+	];
 
-  const coordinates = [
-    { top: "0px", left: "0px" },
-    { top: "0px", left: "500px" },
-    { top: "0px", left: "1000px" },
-    { top: "500px", left: "0px" },
-    { top: "500px", left: "500px" },
-    { top: "500px", left: "1000px" },
-    { top: "1000px", left: "0px" },
-    { top: "1000px", left: "500px" },
-    { top: "1000px", left: "1000px" },
-    { top: "1500px", left: "0px" },
-    { top: "1500px", left: "500px" },
-    { top: "1500px", left: "1000px" },
-  ];
-
-  return (
-    <div className="px-10 text-white h-full relative w-full" ref={containerRef}>
-      <h1 className="text-9xl sticky top-40 text-white">MY PROJECTS</h1>
-      <div className="relative gap-20 w-full h-full">
-        {data.projects.data.map((project, i) => (
-          <div
-            ref={(el) => (projectRefs.current[i] = el)}
-            key={i}
-            className="absolute h-[300px] w-[300px] overflow-hidden"
-            style={{ top: coordinates[i].top, left: coordinates[i].left }}
-          >
-            <Image
-              src={project.img}
-              height={1000}
-              width={1000}
-              alt="image"
-              className="h-full w-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="px-10 pt-40 relative text-white  h-[2100px]  w-full"
+			ref={containerRef}
+		>
+			<h1 className="text-9xl  text-white" ref={titleRef}>
+				MY PROJECTS
+			</h1>
+			<div className=" bg-teal-500 gap-20 w-full ">
+				{data.projects.data.map((project, i) => (
+					<div
+						ref={(el) => (projectRefs.current[i] = el)}
+						key={i}
+						className="absolute h-[300px] w-[300px] overflow-hidden"
+						style={{ top: coordinates[i].top, left: coordinates[i].left }}
+					>
+						<Image
+							src={project.img}
+							height={1000}
+							width={1000}
+							alt="image"
+							className="h-full w-full object-cover"
+						/>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
--- app/page.tsx
+++ app/page.tsx
@@ -19,9 +19,7 @@ export default function Home() {
   useEffect(() => {
     const lenis = new Lenis();
 
-    lenis.on("scroll", (e) => {
-      console.log(e);
-    });
+    lenis.on("scroll", (e) => {});
 
     function raf(time) {
       lenis.raf(time);
