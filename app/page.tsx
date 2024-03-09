8f1686a9b8058b2c777266e25e21004b1372695e
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-09 16:50:31 +0530

    removed bg fixed bug

--- app/page.tsx
+++ app/page.tsx
@@ -10,42 +10,27 @@ import { data } from "./lib/data";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
-import { useLayoutEffect, useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-	const containerRef = useRef<HTMLDivElement>(null);
-
-	const imageRef = useRef<HTMLImageElement>(null);
-
-	useLayoutEffect(() => {
-		let ctx = gsap.context(() => {
-			ScrollTrigger.create({
-				trigger: containerRef.current,
-				pin: imageRef.current,
-				start: "top top",
-				end: "bottom bottom",
-				scrub: true,
-				markers: true,
-			});
-		});
-
-		return () => ctx.revert();
-	}, []);
-
-	return (
-		<main className="pb-[1000px]">
-			<div className="z-10 bg-black ">
-				<Hero />
-				<About />
-				<Features />
-				<Skills />
-				<Projects />
-			</div>
-			<div className="-z-10 fixed bottom-0 bg-white left-0 h-full w-full">
-				<Contact />
-			</div>
-		</main>
-	);
+  return (
+    <main className="pb-[100vh]">
+      <div
+        style={{ backgroundImage: `url(${data.personal.img})` }}
+        className="z-10 bg-black bg-fixed bg-cover"
+      >
+        <div className="z-20 ">
+          <Hero />
+          <About />
+          <Features />
+          <Skills />
+          <Projects />
+        </div>
+      </div>
+      <div className="-z-10 fixed bottom-0 bg-white left-0 h-full w-full">
+        <Contact />
+      </div>
+    </main>
+  );
 }
