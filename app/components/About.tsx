185de3c84cb9f5641dce5adbb5cf59217acbadf5
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-09 19:07:11 +0530

    added parallax to about and marquees

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,11 +1,33 @@
 "use client";
 
+import { useLayoutEffect, useRef } from "react";
 import { data } from "../lib/data";
+import gsap from "gsap";
 
 export const About = () => {
-	return (
-		<div className="px-10 w-full">
-			<p className="text-2xl text-white ">{data.about.description}</p>
-		</div>
-	);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const paraRef = useRef<HTMLParagraphElement>(null);
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: 1,
+        },
+      });
+      tl.to(paraRef.current, { y: -200 }, 0);
+    });
+
+    return () => ctx.revert();
+  }, []);
+  return (
+    <div ref={containerRef} className="px-10 w-full">
+      <p ref={paraRef} className="text-2xl text-white ">
+        {data.about.description}
+      </p>
+    </div>
+  );
 };
--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useLayoutEffect, useRef } from "react";
 import Marquee from "./Marquee";
+import gsap from "gsap";
 
 const data = [
   "RESPONSIVE DESIGN",
@@ -11,10 +13,33 @@ const data = [
   "SEARCH ENGINE OPTIMIZTION(SEO)",
 ];
 export const Features = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const marqueeRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  const yValues = [-100, -200, -300];
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: 1,
+        },
+      });
+      tl.from(marqueeRefs.current, { y: (i) => yValues[i] }, 0);
+    });
+
+    return () => ctx.revert();
+  }, []);
   return (
-    <div className="py-40 relative w-full  text-white flex justify-center  overflow-hidden items-center">
+    <div
+      ref={containerRef}
+      className="py-40 relative w-full  text-white flex justify-center  overflow-hidden items-center"
+    >
       <div className=" w-screen top-0 flex flex-col gap-2 ">
-        <div className="w-fit">
+        <div className="w-fit" ref={(el) => (marqueeRefs.current[0] = el)}>
           <Marquee
             word={`${data[0]}#${data[1]}#`}
             size="100px"
@@ -23,7 +48,7 @@ export const Features = () => {
             speed={0.09}
           />
         </div>
-        <div className="w-fit">
+        <div className="w-fit" ref={(el) => (marqueeRefs.current[1] = el)}>
           <Marquee
             word={`${data[2]}#${data[3]}#${data[2]}#${data[3]}#`}
             size="100px"
@@ -32,7 +57,7 @@ export const Features = () => {
             speed={0.1}
           />
         </div>
-        <div className="w-fit">
+        <div className="w-fit" ref={(el) => (marqueeRefs.current[2] = el)}>
           <Marquee
             word={`${data[4]}#${data[5]}#`}
             size="100px"
