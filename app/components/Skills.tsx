9d7c3f7703d95b789bf40f9996629a9a8128fc92
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-09 23:01:06 +0530

    changed skills animation

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -7,27 +7,37 @@ export const Skills = () => {
 	const containerRef = useRef<HTMLDivElement>(null);
 	const nameRefs = useRef<(HTMLHeadingElement | null)[]>([]);
 
-	useLayoutEffect(() => {
+	const yValues = [
+		-100, -200, -300, -100, -200, -300, -100, -200, -300, -100, -200, -300,
+		-100, -200, -300, -100,
+	];
+
+	const scaleValues = [
+		1.2, 1.5, 1.8, 2.5, 2, 1.5, 1, 1.7, 1.5, 2.5, 2.6, 1, 0.7, 1.9, 2.3, 2,
+	];
+
+	useEffect(() => {
 		let ctx = gsap.context(() => {
 			const tl = gsap.timeline();
-			gsap.from(barRefs.current, {
+			const barTween = gsap.from(barRefs.current, {
+				paused: true,
 				yPercent: 120,
 				stagger: 0.05,
-				scrollTrigger: {
-					trigger: containerRef.current,
-					start: "top center",
-					end: "top center",
-				},
 			});
-			gsap.from(nameRefs.current, {
-				delay: 1,
-				opacity: 0,
-				stagger: 0.05,
+			function playBar() {
+				barTween.play();
+			}
+
+			tl.from(nameRefs.current, {
+				y: (i) => yValues[i],
+				scale: (i) => scaleValues[i],
 				scrollTrigger: {
 					trigger: containerRef.current,
 					start: "top center",
-					end: "top center",
+					end: "top 30%",
+					scrub: true,
 				},
+				onComplete: playBar,
 			});
 		});
 
@@ -35,13 +45,16 @@ export const Skills = () => {
 	}, []);
 
 	return (
-		<div className="px-10 text-white h-full  w-full" ref={containerRef}>
+		<div
+			className="px-10 text-white h-full  w-full overflow-y-hidden"
+			ref={containerRef}
+		>
 			<div className="flex flex-col gap-4">
 				<div className="flex justify-between w-full ">
 					{data.skills.data.map((skill, i) => (
 						<div
 							key={i}
-							className="text-white flex flex-col gap-2 overflow-hidden items-center justify-end"
+							className="text-white flex flex-col gap-2  items-center justify-end"
 						>
 							<div
 								ref={(el) => (barRefs.current[i] = el)}
@@ -50,7 +63,7 @@ export const Skills = () => {
 							/>
 							<h1
 								ref={(el) => (nameRefs.current[i] = el)}
-								className="flex-shrink-0"
+								className="flex-shrink-0 text-red-500"
 							>
 								{skill.name}
 							</h1>
