9fd0c67c49a91e1267fb2a1722baccd767e7af19
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-10 19:14:08 +0530

    added navbar design

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
+
 export const Navbar = () => {
-  return (
-    <div className="w-full p-10 text-white flex justify-between fixed top-0 items-center">
-      <h1 className="text-3xl font-semibold">HRISHIK SHAJI.</h1>
-      <div className="flex gap-4 items-center">
-        <button className="p-2 rounded-md bg-white text-black">DARK</button>
-        <button className="p-2 rounded-md bg-white text-black">MENU</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full p-10 text-white flex z-50 justify-between mix-blend-difference bg-black fixed top-0">
+			<h1 className="text-3xl font-semibold ">HRISHIK SHAJI.</h1>
+			<div className="flex flex-col bg-white text-black rounded-md   p-2">
+				<Link href="#about">ABOUT</Link>
+				<Link href="#projects">PROJECTS</Link>
+				<Link href="#contact">CONTACT</Link>
+			</div>
+		</div>
+	);
 };
