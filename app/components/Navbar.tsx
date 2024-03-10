09c27100210d0db0788912512eb0326e7e56c636
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-10 22:38:05 +0530

    hover animation works

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,47 @@
+"use client";
+
+import gsap from "gsap";
 import Link from "next/link";
+import { useEffect, useLayoutEffect, useRef } from "react";
 
 export const Navbar = () => {
-  return (
-    <div className="w-full p-10 text-white flex z-50 justify-between mix-blend-difference bg-black fixed top-0">
-      <h1 className="text-3xl font-semibold font-righteous">HRISHIK </h1>
-      <div className="flex flex-col bg-white text-black rounded-md font-poppins font-semibold  p-2">
-        <Link href="#about">ABOUT</Link>
-        <Link href="#projects">PROJECTS</Link>
-        <Link href="#contact">CONTACT</Link>
-      </div>
-    </div>
-  );
+	const ref = useRef<HTMLHeadingElement>(null);
+
+	useEffect(() => {
+		let ctx = gsap.context(() => {
+			gsap.set(ref.current, { yPercent: -100 })
+		})
+
+		return () => ctx.revert()
+	}, []);
+
+	const onEnter = (e: MouseEvent) => {
+		let q = gsap.utils.selector(e.currentTarget);
+		gsap.to(ref.current, { yPercent: 0 })
+		gsap.to(q(".inner"), { yPercent: 100 });
+	};
+	const onLeave = (e: MouseEvent) => {
+		let q = gsap.utils.selector(e.currentTarget);
+		gsap.to(ref.current, { yPercent: -100 })
+		gsap.to(q(".inner"), { yPercent: 0 });
+	};
+
+	return (
+		<div className="w-full p-10 text-white flex z-50 justify-between mix-blend-difference bg-black fixed top-0">
+			<h1 className="text-3xl font-semibold font-righteous">HRISHIK </h1>
+			<div className="flex flex-col bg-white text-black rounded-md font-poppins font-semibold  p-2">
+				<Link
+					href="#about"
+					className="overflow-hidden relative"
+					onMouseEnter={onEnter}
+					onMouseLeave={onLeave}
+				>
+					<h1 className="inner-two absolute" ref={ref}>ABOUT</h1>
+					<h1 className="inner">ABOUT</h1>
+				</Link>
+				<Link href="#projects">PROJECTS</Link>
+				<Link href="#contact">CONTACT</Link>
+			</div>
+		</div>
+	);
 };
