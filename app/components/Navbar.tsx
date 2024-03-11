abd5e5aa9fd341173b0074e0a5ad181770d4ab95
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-11 12:53:13 +0530

    added smooth scroll

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,19 @@
 "use client";
 
+import Link from "next/link";
 import { AnimatedLink } from "./AnimatedLink";
 
 export const Navbar = () => {
-  return (
-    <div className="w-full p-10 text-white flex z-50 justify-between mix-blend-difference bg-black fixed top-0">
-      <h1 className="text-3xl font-semibold font-righteous">HRISHIK </h1>
-      <div className="flex flex-col bg-white text-black rounded-md font-poppins font-semibold  p-2">
-        <AnimatedLink href="#about" label="ABOUT" />
-        <AnimatedLink href="#projects" label="PROJECTS" />
-        <AnimatedLink href="#contact" label="CONTACT" />
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full p-10 text-white flex z-50 justify-between mix-blend-difference bg-black fixed top-0">
+			<Link href="#home">
+				<h1 className="text-3xl font-semibold font-righteous">HRISHIK </h1>
+			</Link>
+			<div className="flex flex-col bg-white text-black rounded-md font-poppins font-semibold  p-2">
+				<AnimatedLink href="#about" label="ABOUT" />
+				<AnimatedLink href="#projects" label="PROJECTS" />
+				<AnimatedLink href="#contact" label="CONTACT" />
+			</div>
+		</div>
+	);
 };
--- app/page.tsx
+++ app/page.tsx
@@ -5,34 +5,32 @@ import { About } from "./components/About";
 import { Features } from "./components/Features";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
-import Image from "next/image";
 import { data } from "./lib/data";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
-import { useEffect, useState } from "react";
-import { Preloader } from "./components/Preloader";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-  return (
-    <main className="pb-[100vh]">
-      <div
-        style={{ backgroundImage: `url(${data.personal.img})` }}
-        className="z-10 bg-black bg-fixed bg-cover"
-      >
-        <div className="z-20 ">
-          <Hero />
-          <About />
-          <Features />
-          <Skills />
-          <Projects />
-        </div>
-      </div>
-      <div className="-z-10 fixed bottom-0 bg-white left-0 h-full w-full">
-        <Contact />
-      </div>
-    </main>
-  );
+	return (
+		<main id="home" className="pb-[100vh]">
+			<div
+				style={{ backgroundImage: `url(${data.personal.img})` }}
+				className="z-10 bg-black bg-fixed bg-cover"
+			>
+				<div className="z-20 ">
+					<Hero />
+					<About />
+					<Features />
+					<Skills />
+					<Projects />
+				</div>
+				<div id="contact"></div>
+			</div>
+			<div className="-z-10 fixed bottom-0 bg-white left-0 h-full w-full">
+				<Contact />
+			</div>
+		</main>
+	);
 }
